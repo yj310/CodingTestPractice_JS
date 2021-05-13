85aa9e2faba211f3428cb0adfb08271546ca64df
lyj310 <yoenjii310@gmail.com>
2021-05-13 10:06:12 +0900

    Valnila JS - 2

--- VanilaJS/index.js
+++ VanilaJS/index.js
@@ -1,1 +1,8 @@
-alert('Im Working. Im JS. Im Beautiful. Im worth it.');
+/*alert('Im Working. Im JS. Im Beautiful. Im worth it.');
+console.log('Im Working. Im JS. Im Beautiful. Im worth it.');
+*/
+
+
+const a = 221;
+let b = a - 5;
+console.log(b);
