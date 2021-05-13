0828782ee05fed271cae4660f0d3669a01cde6f5
lyj310 <yoenjii310@gmail.com>
2021-05-13 15:11:32 +0900

    Vanila JS - 19 Saving the user name

--- VanilaJS/greeting.js
+++ VanilaJS/greeting.js
@@ -2,8 +2,24 @@ const form = document.querySelector(".js-form"),
     input = form.querySelector("input"), 
     greeting = document.querySelector(".js-greetings");
 
-const User_LS = "currentUser", 
+const USER_LS = "currentUser", 
     SHOWING_CN = "showing";
+
+function saveName(text) {
+    localStorage.setItem(USER_LS, text);
+}
+
+function handleSubmit(event) {
+    event.preventDefault();
+    const currentValue = input.value;
+    paintGreeting(currentValue);
+    saveName(currentValue);
+}
+
+function askForName() {
+    form.classList.add(SHOWING_CN);
+    form.addEventListener("submit", handleSubmit);
+}
 
 function paintGreeting(text) {
     form.classList.remove(SHOWING_CN);
@@ -12,12 +28,10 @@ function paintGreeting(text) {
 }
 
 function loadName() {
-    const currentUser = localStorage.getItem(User_LS);
+    const currentUser = localStorage.getItem(USER_LS);
     if(currentUser === null) {
-        // she is not
-
+        askForName();
     } else {
-        // she is
         paintGreeting(currentUser);
     }
 }
