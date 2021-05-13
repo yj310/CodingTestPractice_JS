485d5047427d76e4c0fef1e20096ee153a4abff5
lyj310 <yoenjii310@gmail.com>
2021-05-13 11:49:30 +0900

    Vanila JS - 12

--- VanilaJS/index.js
+++ VanilaJS/index.js
@@ -126,6 +126,7 @@ if(10 === '10') {
 }
 */
 
+/*
 const age = prompt("How old are you");
 
 if(age > 19) {
@@ -133,3 +134,42 @@ if(age > 19) {
 } else {
     console.log("you cant")
 }
+*/
+
+
+const title = document.querySelector("#title");
+const BASE_COLOR = "rgb(52, 73, 94)";
+const OTHER_COLOR = "#7f8c8d";
+
+function handleClick() {
+    
+    const currentColor = title.style.color;
+
+    if(currentColor == BASE_COLOR) {
+        title.style.color = OTHER_COLOR;
+    } else {
+        title.style.color = BASE_COLOR;
+    }
+
+}
+
+function init() {
+    title.style.color = BASE_COLOR;
+    title.addEventListener("click", handleClick);
+    //title.addEventListener("mouseenter", handleClick);
+}
+
+init();
+
+
+
+function handleOffline() {
+    console.log("bye bye");
+}
+
+function handleOnline() {
+    console.log("Welcome back");
+}
+
+window.addEventListener("offline", handleOffline);
+window.addEventListener("online", handleOnline);
