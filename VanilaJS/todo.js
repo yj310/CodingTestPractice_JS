a6bbda0d1d00cad1763929a7a57fbcfed76d2e0b
lyj310 <yoenjii310@gmail.com>
2021-05-14 09:57:37 +0900

    Vanila JS - 21 ToDoList

--- VanilaJS/todo.js
+++ VanilaJS/todo.js
@@ -4,19 +4,35 @@ const toDoForm = document.querySelector(".js-toDoForm"),
 
 const TODOS_LS = 'toDos';
 
+const toDos = [];
+
+
+function saveToDos() {
+    localStorage.setItem(TODOS_LS, JSON.stringify(toDos));
+}
 
 function paintToDo(text) {  
     const li = document.createElement("li");
     const delBtn = document.createElement("button");
     const span = document.createElement("span");
+    const newId = toDos.length + 1;
 
     delBtn.innerText = "❌";
     span.innerText = text;
 
     li.appendChild(delBtn);
     li.appendChild(span);
+    li.id = newId;
 
     toDoList.appendChild(li);
+
+    const toDoObj = {
+        text: text, 
+        id: newId
+    }
+
+    toDos.push(toDoObj);
+    saveToDos();
 
 }
 
@@ -27,11 +43,12 @@ function handleSubmit(event) {
 }
 
 function loadToDo() {
-    const toDos = localStorage.getItem(TODOS_LS);
-    if(toDos === null) {
-        
-    } else {
-
+    const loadToDos = localStorage.getItem(TODOS_LS);
+    if(loadToDos !== null) {
+        const parsedToDos = JSON.parse(loadToDos);
+        parsedToDos.forEach(function(toDo) {
+            paintToDo(toDo.text);
+        });
     }
 }
 
