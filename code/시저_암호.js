f42a08aafbfcbbdaaf423d4ebd46e8e755f3f7ef
lyj310 <yoenjii310@gmail.com>
2021-05-22 23:33:07 +0900

    시저 암호

---
+++ code/시저_암호.js
@@ -0,0 +1,36 @@
+/*
+    어떤 문장의 각 알파벳을 일정한 거리만큼 밀어서 다른 알파벳으로 바꾸는 암호화 방식을 시저 암호라고 합니다. 
+    예를 들어 "AB"는 1만큼 밀면 "BC"가 되고, 3만큼 밀면 "DE"가 됩니다. "z"는 1만큼 밀면 "a"가 됩니다. 
+    문자열 s와 거리 n을 입력받아 s를 n만큼 민 암호문을 만드는 함수, solution을 완성해 보세요.
+*/
+
+function solution(s, n) {
+    var answer = '';
+    var arrayBig = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+    var arraySmall = "abcdefghijklmnopqrstuvwxyz"
+
+    for(var i = 0; i < s.length; i++) {
+        if(arrayBig.indexOf(s[i]) != -1)
+            answer += arrayBig[(arrayBig.indexOf(s[i]) + n) % arraySmall.length];
+        else if(arraySmall.indexOf(s[i]) != -1)
+            answer += arraySmall[(arraySmall.indexOf(s[i]) + n) % arraySmall.length];
+        else 
+            answer += s[i];
+
+    }
+
+    return answer;
+}
+
+
+s = "AB";
+n = 1;
+console.log(solution(s, n));    // "BC"
+
+s = "z";
+n = 1;
+console.log(solution(s, n));    // "a"
+
+s = "a B z";
+n = 4;
+console.log(solution(s, n));    // "e F d"
